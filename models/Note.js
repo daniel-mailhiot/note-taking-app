1ef083fe9c6d309a848a1c68767addc200483ab9
daniel-mailhiot <mailhiot2@gmail.com>
2026-02-14 16:20:06 -0800

    Create Note schema with validation, userId and timestamps

---
+++ models/Note.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        maxlength: 40,
+        trim: true,
+        required: true
+    },
+    content: {
+        type: String,
+        maxlength: 2000,
+        trim: true,
+        required: true
+    },
+    userId: { // will be used to link the note to a user
+        type: String,
+        required: true
+    }
+},
+    {
+        timestamps: true // automatically add createdAt and updatedAt fields to the schema
+    }
+);
+
+export default mongoose.model('Note', noteSchema);
+
+/*
+Notes to self:
+https://mongoosejs.com/docs/schematypes.html
+*/
