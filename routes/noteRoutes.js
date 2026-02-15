e1be6309b1d923732799200a4cbec59b15450767
daniel-mailhiot <mailhiot2@gmail.com>
2026-02-14 19:14:31 -0800

    Add note CRUD routes and verify with browser/Postman tests

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,10 +1,23 @@
 import express from "express";
+import {
+  listNotes,
+  createNewNote,
+  updateNote,
+  deleteNote,
+  redirectToNewNote,
+  redirectToEditNote,
+  redirectToDeleteNote
+} from "../controllers/noteController.js";
 
 const router = express.Router();
 
-// Temp test route
-router.get("/", (req, res) => {
-  res.send("Notes route is working");
-});
+router.get('/', listNotes);
+router.get('/new', redirectToNewNote);
+router.get('/:id/edit', redirectToEditNote);
+router.get('/:id/delete', redirectToDeleteNote);
+
+router.post('/', createNewNote);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
 
 export default router;
