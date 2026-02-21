7de3a0536c60506a215e87bf16bf67de6ad0cfea
Daniel <mailhiot2@gmail.com>
2026-02-20 16:19:36 -0800

    Auth: add session middleware & verify register-login-logout flow, update header to display username for testing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import methodOverride from 'method-override';
+import session from 'express-session';
 
 import { connectDB } from './config/db.js';
 import noteRoutes from './routes/noteRoutes.js';
@@ -20,6 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method')); 
 app.use(express.static('public'));
 app.use(cors());
+
+app.use( // enable req.session in controllers
+  session({
+    secret: process.env.SESSION_SECRET, // secret used to sign the session cookie
+    resave: false, // don't save session if nothing changed
+    saveUninitialized: false // don't create empty sessions for anonymous visitors
+  })
+);
+
+app.use((req, res, next) => { // make logged-in username available in all views for testing
+  res.locals.currentUsername = req.session.username || null;
+  next();
+});
 
 // Routes
 app.use('/notes', noteRoutes);
